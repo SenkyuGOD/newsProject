75eea94857412d7747ebde581757683fe1f3c25e
SenkyuGOD <lolhk467@gmail.com>
2024-06-10 17:30:42 +0300

    123

---
+++ src/main/java/edu/training/web/newsproject/controller/listner/AppContextListener.java
@@ -0,0 +1,17 @@
+package edu.training.web.newsproject.controller.listner;
+
+import edu.training.web.newsproject.dao.connetionpool.ConnectionPool;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+public class AppContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ConnectionPool.getInstance();
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ConnectionPool.getInstance().dispose();
+    }
+}
