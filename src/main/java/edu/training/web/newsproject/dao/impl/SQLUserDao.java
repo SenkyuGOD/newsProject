fdb6723c1e6707205e841031a5509bd87aee6465
SenkyuGOD <lolhk467@gmail.com>
2024-06-27 22:17:04 +0300

    123

--- src/main/java/edu/training/web/newsproject/dao/impl/SQLUserDao.java
+++ src/main/java/edu/training/web/newsproject/dao/impl/SQLUserDao.java
@@ -16,10 +16,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SQLUserDao implements UserDao {
 
     private final ConnectionPool connectionPool = ConnectionPool.getInstance();
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
 
     private static final String SIGN_IN_SQL =
             "SELECT u.idUser, u.username, u.email, u.password, r.title AS role " +
@@ -37,6 +40,7 @@ public class SQLUserDao implements UserDao {
             statement.setString(2, authInfo.getPassword());
 
             try (ResultSet resultSet = statement.executeQuery()) {
+                logger.log(Level.INFO, resultSet.getString("username"));
                 if (resultSet.next()) {
                     int idUser = resultSet.getInt("idUser");
                     String username = resultSet.getString("username");
@@ -52,6 +56,7 @@ public class SQLUserDao implements UserDao {
 
                     return new User(idUser, username, email, role);
                 } else {
+                    logger.log(Level.INFO, "User not found.");
                     throw new DaoException("Invalid username or password.");
                 }
             }
@@ -72,6 +77,7 @@ public class SQLUserDao implements UserDao {
             statement.setString(1, email);
 
             try (ResultSet resultSet = statement.executeQuery()) {
+                logger.log(Level.INFO, resultSet.getString("email"));
                 if (resultSet.next()) {
                     int count = resultSet.getInt(1);
                     if (count > 0) {
@@ -81,6 +87,7 @@ public class SQLUserDao implements UserDao {
             }
 
         } catch (SQLException | ConnectionPoolException e) {
+            logger.log(Level.SEVERE, "Error occurred during checking user existence", e);
             throw new DaoException("Error occurred during check existence", e);
         }
         return false;
@@ -94,6 +101,7 @@ public class SQLUserDao implements UserDao {
         try (Connection connection = connectionPool.takeConnection()) {
             try (PreparedStatement userStmt = connection.prepareStatement(INSERT_USER_SQL);
                  PreparedStatement roleStmt = connection.prepareStatement(INSERT_USER_ROLE_SQL)) {
+                logger.log(Level.INFO, "Inserting user into user_has_role");
 
                 userStmt.setInt(1, userRegInfo.getUserId());
                 userStmt.setString(2, userRegInfo.getUsername());
@@ -113,13 +121,14 @@ public class SQLUserDao implements UserDao {
                 roleStmt.executeUpdate();
 
             } catch (SQLException e) {
+                logger.log(Level.SEVERE, "Error occurred during sign up", e);
                 throw new DaoException("Error occurred during sign up", e);
             }
         } catch (SQLException | ConnectionPoolException e) {
+            logger.log(Level.SEVERE, "Error occurred during sign up", e);
             throw new DaoException("Error occurred during sign up", e);
         }
     }
-
 
 
     private static final String DELETE_USER_SQL = "DELETE FROM user WHERE idUser = ?;";
@@ -149,6 +158,7 @@ public class SQLUserDao implements UserDao {
     public void changeUserRole(int userId, int roleId) throws DaoException {
         try (Connection connection = connectionPool.takeConnection();
              PreparedStatement statement = connection.prepareStatement(CHANGE_USER_ROLE_SQL)) {
+            logger.log(Level.INFO, "Changing user role to " + roleId);
 
             statement.setInt(1, roleId);
             statement.setInt(2, userId);
@@ -156,10 +166,12 @@ public class SQLUserDao implements UserDao {
             int affectedRows = statement.executeUpdate();
 
             if (affectedRows == 0) {
+                logger.log(Level.SEVERE, "Changing user role failed, no rows affected.");
                 throw new DaoException("Changing user role failed, no rows affected.");
             }
 
         } catch (SQLException | ConnectionPoolException e) {
+            logger.log(Level.SEVERE, "Error occurred during changing user role", e);
             throw new DaoException("Error occurred during changing user role", e);
         }
     }
@@ -170,6 +182,7 @@ public class SQLUserDao implements UserDao {
     @Override
     public int getRoleId(String roleTitle) throws DaoException {
         try (Connection connection = connectionPool.takeConnection(); PreparedStatement statement = connection.prepareStatement(GET_ROLE_ID)) {
+            logger.log(Level.INFO, "Getting role id for " + roleTitle);
 
             statement.setString(1, roleTitle);
 
@@ -178,11 +191,13 @@ public class SQLUserDao implements UserDao {
                 if (resultSet.next()) {
                     return resultSet.getInt("idrole");
                 } else {
+                    logger.log(Level.SEVERE, "No role found for " + roleTitle);
                     throw new SQLException("Role not found: " + roleTitle);
                 }
             }
 
         } catch (ConnectionPoolException | SQLException e) {
+            logger.log(Level.SEVERE, "Error occurred during getting role id for " + roleTitle, e);
             throw new DaoException("Error occurred during getting role id", e);
         }
     }
@@ -193,6 +208,7 @@ public class SQLUserDao implements UserDao {
     public void changeUserPassword(int id, String newPassword) throws DaoException {
         try (Connection connection = connectionPool.takeConnection();
              PreparedStatement statement = connection.prepareStatement(CHANGE_USER_PASSWORD_SQL)) {
+            logger.log(Level.INFO, "Changing user password to " + newPassword);
 
             statement.setString(1, newPassword);
             statement.setInt(2, id);
@@ -200,10 +216,12 @@ public class SQLUserDao implements UserDao {
             int affectedRows = statement.executeUpdate();
 
             if (affectedRows == 0) {
+                logger.log(Level.SEVERE, "Changing user password failed, no rows affected.");
                 throw new DaoException("Changing user password failed, no rows affected.");
             }
 
         } catch (SQLException | ConnectionPoolException e) {
+            logger.log(Level.SEVERE, "Error occurred during changing user password", e);
             throw new DaoException("Error occurred during changing user password", e);
         }
     }
@@ -212,14 +230,16 @@ public class SQLUserDao implements UserDao {
 
     @Override
     public void updateUserToken(int userId, String token) throws DaoException {
-        try(Connection connection = connectionPool.takeConnection();
-        PreparedStatement statement = connection.prepareStatement(UPDATE_USER_TOKEN_SQL)) {
+        try (Connection connection = connectionPool.takeConnection();
+             PreparedStatement statement = connection.prepareStatement(UPDATE_USER_TOKEN_SQL)) {
+            logger.log(Level.INFO, "Updating user token to " + token);
 
-            statement.setString(1,token);
-            statement.setInt(2,userId);
+            statement.setString(1, token);
+            statement.setInt(2, userId);
 
             statement.executeUpdate();
         } catch (SQLException | ConnectionPoolException e) {
+            logger.log(Level.SEVERE, "Error occurred during updating user token", e);
             throw new DaoException("Error occurred during updating user token", e);
         }
 
@@ -230,12 +250,13 @@ public class SQLUserDao implements UserDao {
 
     @Override
     public User findUserByToken(String token) throws DaoException {
-        try(Connection connection = connectionPool.takeConnection();
-        PreparedStatement statement = connection.prepareStatement(FIND_USER_BY_TOKEN_SQL)) {
+        try (Connection connection = connectionPool.takeConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_USER_BY_TOKEN_SQL)) {
+            logger.log(Level.INFO, "Finding user by token " + token);
 
-            statement.setString(1,token);
+            statement.setString(1, token);
 
-            try(ResultSet resultSet = statement.executeQuery()) {
+            try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
                     int idUser = resultSet.getInt("idUser");
                     String username = resultSet.getString("username");
@@ -250,10 +271,12 @@ public class SQLUserDao implements UserDao {
                     }
                     return new User(idUser, username, email, role);
                 } else {
+                    logger.log(Level.SEVERE, "No such user found for token " + token);
                     throw new DaoException("Invalid user token.");
                 }
             }
         } catch (SQLException | ConnectionPoolException e) {
+            logger.log(Level.SEVERE, "Error occurred during finding user", e);
             throw new DaoException("Error occurred during finding user by token", e);
         }
     }
@@ -271,6 +294,7 @@ public class SQLUserDao implements UserDao {
         try (Connection connection = connectionPool.takeConnection();
              PreparedStatement statement = connection.prepareStatement(SELECT_ALL_USERS_SQL);
              ResultSet resultSet = statement.executeQuery()) {
+            logger.log(Level.INFO, "Getting all users from database");
 
             while (resultSet.next()) {
                 int idUser = resultSet.getInt("idUser");
@@ -284,6 +308,7 @@ public class SQLUserDao implements UserDao {
             }
 
         } catch (SQLException | ConnectionPoolException e) {
+            logger.log(Level.SEVERE, "Error occurred during getting all users", e);
             throw new DaoException("Error occurred during fetching all users", e);
         }
 
--- src/main/java/edu/training/web/newsproject/service/impl/NewsServiceImpl.java
+++ src/main/java/edu/training/web/newsproject/service/impl/NewsServiceImpl.java
@@ -9,15 +9,20 @@ import edu.training.web.newsproject.service.ServiceException;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NewsServiceImpl implements NewsService {
     private final NewsDao newsDao = DaoProvider.getInstance().getNewsDao();
+    private final Logger logger = Logger.getLogger(NewsServiceImpl.class.getName());
 
     @Override
     public void createNews(News news) throws ServiceException {
         try {
+            logger.log(Level.INFO, "create news");
             newsDao.createNews(news);
         } catch (DaoException e) {
+            logger.log(Level.SEVERE, "createNews error", e);
             throw new ServiceException(e);
         }
     }
@@ -25,8 +30,10 @@ public class NewsServiceImpl implements NewsService {
     @Override
     public void updateNews(News news) throws ServiceException {
         try {
+            logger.log(Level.INFO, "update news");
             newsDao.updateNews(news);
         } catch (DaoException e) {
+            logger.log(Level.SEVERE, "updateNews error", e);
             throw new ServiceException(e);
         }
     }
@@ -35,6 +42,7 @@ public class NewsServiceImpl implements NewsService {
     @Override
     public News getNewsById(Long id) throws ServiceException {
         try {
+            logger.log(Level.INFO, "getNewsById");
             List<News> newsList = newsDao.getAllNews();
             News news1 = null;
             for (News news : newsList) {
@@ -44,6 +52,7 @@ public class NewsServiceImpl implements NewsService {
             }
             return news1;
         } catch (DaoException e) {
+            logger.log(Level.SEVERE, "getNewsById error", e);
             throw new ServiceException(e);
         }
     }
@@ -51,6 +60,7 @@ public class NewsServiceImpl implements NewsService {
     @Override
     public News getNewsByTitle(String title) throws ServiceException {
         try {
+            logger.log(Level.INFO, "getNewsByTitle");
             List<News> newsList = newsDao.getAllNews();
             News news1 = null;
             for (News news : newsList) {
@@ -60,6 +70,7 @@ public class NewsServiceImpl implements NewsService {
             }
             return news1;
         } catch (DaoException e) {
+            logger.log(Level.SEVERE, "getNewsByTitle error", e);
             throw new ServiceException(e);
         }
     }
@@ -67,8 +78,10 @@ public class NewsServiceImpl implements NewsService {
     @Override
     public List<News> getAllNews() throws ServiceException {
         try {
+            logger.log(Level.INFO, "getAllNews");
             return newsDao.getAllNews();
         } catch (DaoException e) {
+            logger.log(Level.SEVERE, "getAllNews error", e);
             throw new ServiceException(e);
         }
     }
